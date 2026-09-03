6f65503ae1a64f36f7b28afe0894d3bd30837e64
zhoucheng <zhoucheng133@outlook.com>
2026-09-03 21:07:00 +0800

    添加解析heic和heif文件

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module photoarchiver
 
-go 1.24
+go 1.24.0
 
 require github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 
@@ -8,4 +8,5 @@ require (
 	github.com/agnivade/levenshtein v1.2.1 // indirect
 	github.com/andreiashu/geobed v0.0.0-20260212135017-f4489445fc48 // indirect
 	github.com/golang/geo v0.0.0-20260129164528-943061e2742c // indirect
+	go4.org v0.0.0-20260112195520-a5071408f32f // indirect
 )
--- utils/core.go
+++ utils/core.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -9,6 +11,7 @@ import (
 
 	"github.com/andreiashu/geobed"
 	"github.com/rwcarlsen/goexif/exif"
+	"go4.org/media/heif"
 )
 
 var g *geobed.GeoBed
@@ -32,7 +35,8 @@ func GetPhoto(path string) types.Photo {
 		return types.Photo{}
 	}
 	defer f.Close()
-	x, err := exif.Decode(f)
+
+	x, err := decodeEXIF(f, path)
 	if err != nil || x == nil {
 		return types.Photo{}
 	}
@@ -47,6 +51,34 @@ func GetPhoto(path string) types.Photo {
 		Country:  country,
 		City:     city,
 	}
+}
+
+func decodeEXIF(f *os.File, path string) (*exif.Exif, error) {
+	ext := strings.ToLower(filepath.Ext(path))
+
+	switch ext {
+	case ".heic", ".heif":
+		return decodeHEIFEXIF(f)
+	default:
+		return exif.Decode(f)
+	}
+}
+
+func decodeHEIFEXIF(f *os.File) (*exif.Exif, error) {
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	reader := io.NewSectionReader(f, 0, stat.Size())
+
+	h := heif.Open(reader)
+
+	rawEXIF, err := h.EXIF()
+	if err != nil {
+		return nil, err
+	}
+
+	return exif.Decode(bytes.NewReader(rawEXIF))
 }
 
 func getCaptureTime(exifData *exif.Exif) string {
